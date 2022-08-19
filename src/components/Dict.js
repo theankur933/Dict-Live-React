9038c8d7d151bc6a97a64efda2cc164ab40227c4
Ankur Kharwar <ankurkharwar933@gmail.com>
2022-08-19 22:20:51 +0530

    first commit

---
+++ src/components/Dict.js
@@ -0,0 +1,79 @@
+import React from "react-dom";
+const Dict = () => {
+  return (
+    <>
+      <div class="container mt-3">
+        <h3 class="text-center text-info">
+          <u>Some Word Meanings are given below:</u>
+        </h3>
+        <table class="table table-hover text-center">
+          <tr>
+            <th>Words</th>
+            <th>Meanings</th>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">1. </b>Ran away
+            </td>
+            <td>भाग गया</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">2. </b>Tremble
+            </td>
+            <td>घबराना</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">3. </b>Shrieked
+            </td>
+            <td>चीख</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">4. </b>Applauded
+            </td>
+            <td>सराहना</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">5. </b>Reunite
+            </td>
+            <td>पुनर्मिलन</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">6. </b>Erratically
+            </td>
+            <td>अनिश्चित</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">7. </b>Go away
+            </td>
+            <td>दूर जाओ</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">8. </b>Affirmation
+            </td>
+            <td>प्रतिज्ञान</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">9. </b>Shuddered
+            </td>
+            <td>थरथराता हुआ</td>
+          </tr>
+          <tr>
+            <td>
+              <b class="text-info">10. </b>Cocooned
+            </td>
+            <td>कोकून</td>
+          </tr>
+        </table>
+      </div>
+    </>
+  );
+};
+export default Dict;
